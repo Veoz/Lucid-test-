b73431962d0c24d22f7a83c3ec009aafaf77b930
Veoz <VEoz.id@gmail.com>
2020-06-29 14:48:51 +0300

    done

--- app/menu.js
+++ app/menu.js
@@ -1,1 +1,8 @@
 
+document.querySelector('.menu-icon-wrapper').onclick = function(){
+    document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
+    document.querySelector('.rm').classList.toggle('menu-active');
+
+
+
+}
